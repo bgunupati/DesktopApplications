14855dc36cc9f133a1b7308f383943b66a0818d9
bgunupati <bgunupati@gmail.com>
2017-11-06 20:37:55 -0500

    Added Yeild Function

--- YeildExample/YeildExample/Program.cs
+++ YeildExample/YeildExample/Program.cs
@@ -8,8 +8,35 @@ namespace YeildExample
 {
     class Program
     {
+        static List<int> intList = new List<int>();
+
+
+        static void AddValuesInList()
+        {
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+            intList.Add(4);
+            intList.Add(5);
+            intList.Add(6);
+        }
+
         static void Main(string[] args)
         {
+            AddValuesInList();
+            foreach (int i in WtihYeild())
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
+
+        static IEnumerable<int> WtihYeild()
+        {
+            foreach (int i in intList)
+            {
+                if (i > 4) yield return i;
+            }
+        } 
     }
 }
